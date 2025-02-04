1f07418d5bcc27dba06ac815036021ccb31d0acc
CrisisPena1 <garcia.cristina.1fm@gmail.com>
2025-02-04 13:27:42 -0700

    Primer commit para GitHub Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GaleriaComponent
   ],
   imports: [
     BrowserModule,
